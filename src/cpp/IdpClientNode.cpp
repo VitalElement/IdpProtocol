d94b29f18ea3020da5e9324223f4bc7746bf579b
Dan Walmsley <dan@walms.co.uk>
2018-07-11 11:57:26 +0100

    add an implementation for IdpClients.

---
+++ src/cpp/IdpClientNode.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+ *  Description:
+ *
+ *       Author:
+ *
+ ******************************************************************************/
+
+#include "IdpClientNode.h"
+
+IdpClientNode::IdpClientNode (Guid_t guid, const char* name, uint16_t address)
+    : IdpNode (guid, name, address)
+{
+    _clientAddress = UnassignedAddress;
+
+    Manager ().RegisterCommand (static_cast<uint16_t> (ClientCommand::Connect),
+                                [&](std::shared_ptr<IncomingTransaction> i,
+                                    std::shared_ptr<OutgoingTransaction> o) {
+                                    _clientAddress = i->Source ();
+
+                                    ClientConnected (this);
+
+                                    return IdpResponseCode::OK;
+                                });
+
+    Manager ().RegisterCommand (
+        static_cast<uint16_t> (ClientCommand::Disconnect),
+        [&](std::shared_ptr<IncomingTransaction> i,
+            std::shared_ptr<OutgoingTransaction> o) {
+            _clientAddress = UnassignedAddress;
+
+            ClientDisconnected (this);
+
+            return IdpResponseCode::OK;
+        });
+}
+
+IdpClientNode::~IdpClientNode ()
+{
+}
+
+bool IdpClientNode::IsClientConnected ()
+{
+    return _clientAddress != UnassignedAddress;
+}
+
+uint16_t IdpClientNode::ClientAddress ()
+{
+    return _clientAddress;
+}
---
+++ src/cpp/IdpClientNode.h
@@ -0,0 +1,41 @@
+/******************************************************************************
+ *  Description:
+ *
+ *       Author:
+ *
+ *******************************************************************************/
+#pragma once
+
+#include "IdpNode.h"
+#include <stdbool.h>
+#include <stdint.h>
+
+enum class ClientCommand
+{
+    Connect = 0xD000,
+    Disconnect
+};
+
+/**
+ *  IdpClientNode
+ */
+class IdpClientNode : public IdpNode
+{
+    uint16_t _clientAddress;
+
+  public:
+    /**
+     * Instantiates a new instance of IdpClientNode
+     */
+    IdpClientNode (Guid_t guid, const char* name,
+                   uint16_t address = UnassignedAddress);
+
+    virtual ~IdpClientNode ();
+
+    Event ClientConnected;
+    Event ClientDisconnected;
+
+  protected:
+    bool IsClientConnected ();
+    uint16_t ClientAddress ();
+};
